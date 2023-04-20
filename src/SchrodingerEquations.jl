dcb0ccd1fe4a317343b9a0daaf730c662ca857cb
Rahul Manavalan <rahul.learns.physics@ gmail.com>
2023-04-20 18:10:57 +0200

    Files generated by PkgTemplates
    PkgTemplates version: 0.7.33

---
+++ src/SchrodingerEquations.jl
@@ -0,0 +1,5 @@
+module SchrodingerEquations
+
+# Write your package code here.
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using SchrodingerEquations
+using Test
+
+@testset "SchrodingerEquations.jl" begin
+    # Write your tests here.
+end
